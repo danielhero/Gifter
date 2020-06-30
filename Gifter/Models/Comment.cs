9b923f168bf48a1f1502b49a5efdaf998819ee68
Daniel Hero <danielhero@outlook.com>
2020-06-30 15:08:23 -0500

    added comment entity

---
+++ Gifter/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+
+        public UserProfile UserProfile { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+
+        public Post Post { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
---
+++ Gifter/Repository/CommentRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comment.Include(c => c.UserProfile)
+                                   .Include(c => c.Post)
+                                   .ToList();
+        }
+
+        public Comment GetById(int id)
+        {
+            return _context.Comment.Include(c => c.UserProfile)
+                                   .Include(c => c.Post)
+                                   .FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<Comment> GetByUserProfileId(int id)
+        {
+            return _context.Comment.Include(c => c.UserProfile)
+                                   .Where(c => c.UserProfileId == id)
+                                   .ToList();
+        }
+
+        public List<Comment> GetByPostId(int id)
+        {
+            return _context.Comment.Include(c => c.Post)
+                                   .Where(c => c.PostId == id)
+                                   .ToList();
+        }
+
+        public void Add(Comment comment)
+        {
+            _context.Add(comment);
+            _context.SaveChanges();
+        }
+
+        public void Update(Comment comment)
+        {
+            _context.Entry(comment).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var comment = GetById(id);
+            _context.Comment.Remove(comment);
+            _context.SaveChanges();
+        }
+    }
+}
